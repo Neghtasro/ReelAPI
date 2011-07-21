962423976336dd385c58606699967612068a3970
John Shaw <johnshaw511@gmail.com>
2011-07-21 15:55:19 -0400

    Added the toXMLString function for better compatibility with web frameworks. Revamped the functionality of the toXML function to be usable with both methods of XML generation.

--- XMLGen.py
+++ XMLGen.py
@@ -1,19 +1,32 @@
 #XML Generation Module
-def toXML(f, movie, i):
-    f.write("\t<movie" + str(i) + ">\n")
-    f.write("\t<title>\n\t\t" + movie[0] + "\n\t</title>\n\t<dates>\n\t\t" + movie[1] + "\n\t</dates>")
-    f.write("\t</movie" + str(i) + ">\n")
+def toXML(movie, i):
+    return ("\t<movie" + str(i) + ">\n") + \
+	("\t<title>\n\t\t" + movie[0] + "\n\t</title>\n\t<dates>\n\t\t" + movie[1] + "\n\t</dates>") + \
+	("\t</movie" + str(i) + ">\n")
 
 def MakeXMLFile(movies):
     """Generates the basic XML file, and calls the routine to generate XML for each listing."""
     
-    f = open("movielist.xml", 'w')
+    f = open("/output/movielist.xml", 'w')
     f.write("<?xml version=\"1.0\"?>\n<movielist>\n")
     i = 0
     
     for item in movies:
         i += 1
-        toXML(f, item, i)
+        f.write(toXML(item, i))
     
     f.write("</movielist>")
     f.close()
+	
+def MakeXMLString(movies):
+    """Generates a string, which will form an XML file when parsed. Hopefully."""
+    
+    str = "<?xml version=\"1.0\"?>\n<movielist>\n"
+    i = 0
+    
+    for item in movies:
+        i += 1
+        str += toXML(item, i)
+    
+    str += ("</movielist>")
+    return str
