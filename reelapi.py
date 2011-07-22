a9c1de0dbd2f7fa3c711551fe3f2f0305940768b
John Shaw <johnshaw511@gmail.com>
2011-07-21 22:30:11 -0400

    Added command-line arguments: Can request JSON formatting(not yet implemented) or manually request a month.

--- reelapi.py
+++ reelapi.py
@@ -1,18 +1,28 @@
 import RetrieveMovieList
 import XMLGen
+import sys
 
-month = RetrieveMovieList.findDate()
+xml = True
+month = ""
+for item in sys.argv:
+	if item == "json":
+		xml = False
+	elif (RetrieveMovieList.monthCheck(item) == True):
+		month = item
+
+if month == "":
+	month = RetrieveMovieList.findDate()
 
 #month = "april" #For debugging purposes
-
-if not(month == "break"):
-	movielist = RetrieveMovieList.scraper(month)
-	output = XMLGen.MakeXMLString(movielist, month)
-elif month=="break":
-	output = XMLGen.MakeXMLError("The Reel is currently not open. Try again during the school year.")
-else:
-	output = XMLGen.MakeXMLError("An unknown error occurred.")
-
-print "Content-Type: text/xml"
-print ""
-print output
+if xml == True:
+	if not(month == "break"):
+		movielist = RetrieveMovieList.scraper(month)
+		output = XMLGen.MakeXMLString(movielist, month)
+	elif month=="break":
+		output = XMLGen.MakeXMLError("The Reel is currently not open. Try again during the school year.")
+	else:
+		output = XMLGen.MakeXMLError("An unknown error occurred.")
+	
+	print "Content-Type: text/xml"
+	print ""
+	print output
