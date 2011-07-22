1c7996fd8aee6dfb20a37e2d02415f729a730721
John Shaw <johnshaw511@gmail.com>
2011-07-21 21:34:23 -0400

    Implemented error cases. No longer defaults to April, instead uses the current month.

--- reelapi.py
+++ reelapi.py
@@ -1,14 +1,18 @@
 import RetrieveMovieList
 import XMLGen
 
-#month = findDate()
+month = RetrieveMovieList.findDate()
 
-month = "april" #For debugging purposes
+#month = "april" #For debugging purposes
 
 if not(month == "break"):
 	movielist = RetrieveMovieList.scraper(month)
 	output = XMLGen.MakeXMLString(movielist, month)
+elif month=="break":
+	output = XMLGen.MakeXMLError("The Reel is currently not open. Try again during the school year.")
+else:
+	output = XMLGen.MakeXMLError("An unknown error occurred.")
 
-	print "Content-Type: text/xml"
-	print ""
-	print output
+print "Content-Type: text/xml"
+print ""
+print output
